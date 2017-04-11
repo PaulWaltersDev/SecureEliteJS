958c8042abe8c69d11322193e3e7eb39bc9215df
Paul Walters <paulwalters2002@yahoo.co.uk>
2017-04-11 21:43:05 +1000

    final upload

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ app.set('view engine','ejs');
 
 var routes = require('./routes');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // home
 
@@ -15,6 +18,12 @@ app.get('/', routes.home);
 app.get('/about', routes.about);
 
 app.get('/products', routes.products);
+
+app.get('/protectfx', routes.protectfx);
+
+app.get('/contact', routes.contact);
+
+app.post('/demo_register', routes.demo_register);
 
 //app.get('/about', routes.about);
 //app.get('/demo', routes.demo);
--- routes/index.js
+++ routes/index.js
@@ -9,3 +9,19 @@ exports.about = function(req, res){
 exports.products = function(req, res){
     res.render('products');
 };
+
+exports.protectfx = function(req, res){
+    res.render('protectfx');
+};
+
+exports.demo_register = function(req, res){
+    console.log(req.body);
+    res.render('demo_register', {
+        first_name : req.body.first_name,
+        last_name : req.body.surname
+    });
+};
+
+exports.contact = function(req, res){
+    res.render('contact');
+};
